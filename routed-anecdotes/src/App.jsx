ccb4a2fce7034bef50de2f04d0fb1e8a37a16cc9
Reinout Schols <reinoutschols@hotmail.com>
2024-01-25 09:38:32 +0100

    Completed exercise 7.4

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -4,6 +4,8 @@ import {
   Routes, Route, Link, useParams, useNavigate
 } from 'react-router-dom'
 
+import {useField } from './hooks/index'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -53,7 +55,7 @@ const About = () => (
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      An anecdote is a story with a point.</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
@@ -68,17 +70,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log("logging content in handle submit:", content)
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate("/")
@@ -90,15 +93,21 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input 
+            {...content}
+          />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input
+            {...author}
+          />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input
+            {...info}
+          />
         </div>
         <button>create</button>
       </form>
